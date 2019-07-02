ba8e33faa25257410d206fb9dccc4b3fa11b2b6b
residenti <n.queue.r@gmail.com>
2019-07-03 00:21:23 +0900

    api request

---
+++ app/controllers/api/v1/restaurants_controller.rb
@@ -0,0 +1,42 @@
+module Api
+  module V1
+    class RestaurantsController < ApplicationController
+
+      # TODO
+      # エラーハンドリング ERROR 用クラス作成する.
+      # base_url とかは config/ 配下で管理する.
+      # https リクエストの処理もどこかにまとめる. lib/ 配下とか?
+      # keyid は Git管理下から外して環境変数として読み込む.
+      def show
+
+        # hash形式でパラメタ文字列を指定し、URL形式にエンコード
+        params = URI.encode_www_form(
+          {
+            keyid: 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
+            hit_per_page: 1,
+            input_coordinates_mode: 1,
+            latitude: 35.726973099999995,
+            longitude: 139.521121,
+            range: 5,
+            late_lunch: 1,
+            lunch: 1
+          }
+        )
+
+        uri = URI.parse("https://api.gnavi.co.jp/RestSearchAPI/v3/?#{params}") # URIを解析し、hostやportをバラバラに取得できるようにする
+
+        http = Net::HTTP.new(uri.host, uri.port)
+        http.use_ssl = true # https通信を許可.
+        http.verify_mode = OpenSSL::SSL::VERIFY_NONE # 証明書なんたらかんたら...
+
+        res = http.get(uri.request_uri)
+
+        restaurant = JSON.parse(res.body)
+
+        render(status: :ok, json: { result: { status: 0 }, restaurant: restaurant })
+
+      end
+
+    end
+  end
+end
