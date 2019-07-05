ca806692bfa1b254408a457ea0f46989276b1a09
residenti <n.queue.r@gmail.com>
2019-07-06 00:18:27 +0900

    return restaurant info at random.

--- app/controllers/api/v1/restaurants_controller.rb
+++ app/controllers/api/v1/restaurants_controller.rb
@@ -3,11 +3,18 @@ module Api
     class RestaurantsController < ApiController
       include HttpRequest
 
-      def show
+      KEY_LIST = [
+        "name", "name_kana", "latitude", "longitude", "category", "url",
+        "url_mobile", "coupon_url", "image_url", "address", "tel", "opentime",
+        "holiday", "parking_lots", "pr", "lunch", "credit_card", "e_money"
+      ]
 
+      def show
         res = get_request(35.726973099999995, 139.521121, 5, 1)
 
-        restaurant = JSON.parse(res.body)
+        res_body_json = JSON.parse(res.body) #TODO 0件の時を考慮した処理.
+
+        restaurant = res_body_json["rest"].sample.select { |key| KEY_LIST.include?(key) }
 
         render(status: :ok, json: { result: { status: 0 }, restaurant: restaurant })
 
