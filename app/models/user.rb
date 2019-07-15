f9310d515f7f37c3d3133e6ba2e83463a3652900
residenti <n.queue.r@gmail.com>
2019-07-15 21:07:52 +0900

    introduce devise.
    introduce has_secure_token.

---
+++ app/models/user.rb
@@ -0,0 +1,11 @@
+class User < ApplicationRecord
+
+  has_secure_token :access_token
+
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable,
+         :confirmable, :lockable, :timeoutable, :trackable
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
+
+  devise_for :users
+
+  root 'welcom#manual'
+
   namespace 'api' do
     namespace 'v1' do
       get '/restaurant', to: 'restaurants#show'
     end
   end
+
 end
