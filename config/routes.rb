69d7f1fa7c77c5830c30e4df5036c08acc012cdd
residenti <n.queue.r@gmail.com>
2019-07-16 23:30:26 +0900

    setting for displaying views and csrf protection.

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 Rails.application.routes.draw do
 
-  devise_for :users
+  devise_for :users, :controllers => {
+    :sessions      => "users/sessions",
+    :registrations => "users/registrations",
+    :passwords     => "users/passwords",
+    :confirmations     => "users/confirmations",
+    :unlocks     => "users/unlocks"
+  }
 
-  root 'welcom#manual'
+  root 'application#root' # TODO 仕様書画面作成してそこを指定する.
 
   namespace 'api' do
     namespace 'v1' do
