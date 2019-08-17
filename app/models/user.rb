b2f90634d48bca9745bba2bbb433c660546b939b
residenti <n.queue.r@gmail.com>
2019-08-18 00:22:59 +0900

    [wip] user edit

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,6 +10,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable,
          :confirmable, :lockable, :timeoutable, :trackable
 
+  # TODO バリデーションの追加: 半角英数字(小文字)
   validates :userid, presence: true, uniqueness: true, length: { maximum: 20 }
 
 end
